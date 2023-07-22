a6ed0ef73644c0d3fcccf0eb09271b3356e5138f
patrice02 <patricedev02@gmail.com>
2023-07-22 15:09:25 +0000

    update task 3

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,41 +6,35 @@
  * @second: secont element
  */
 
- void swap(int *first, int *second)
- {
+void swap(int *first, int *second)
+{
 	size_t temp;
 
 	temp = *first;
 	*first = *second;
 	*second = temp;
- }
+}
 
- /**
-  * selection_sort - function that sorts an array of integers in ascending
-  * order using the Selection sort algorithm
-  * @array: array contains integer value
-  * @size: array size
-  */
-
- void selection_sort(int *array, size_t size)
- {
- 	size_t i, j, min;
-
-	if (!array)
-		return;
+/**
+ * selection_sort - function that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @array: array contains integer value
+ * @size: array size
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
-		{
 			if (array[j] < array[min])
 				min = j;
-			if (min != i)
-			{
-				swap(&array[min], &array[i]);
-				print_array(array, size);
-			}
+		if (min != i)
+		{
+			swap(&array[min], &array[i]);
+			print_array(array, size);
 		}
 	}
- }
+}
