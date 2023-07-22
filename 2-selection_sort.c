e8462d01024ff6f2368d9247fa24ef19b4bd8148
patrice02 <patricedev02@gmail.com>
2023-07-22 14:53:28 +0000

    add Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap - helper function use to swap two elements
+ * @first: first element
+ * @second: secont element
+ */
+
+ void swap(int *first, int *second)
+ {
+	size_t temp;
+
+	temp = *first;
+	*first = *second;
+	*second = temp;
+ }
+
+ /**
+  * selection_sort - function that sorts an array of integers in ascending
+  * order using the Selection sort algorithm
+  * @array: array contains integer value
+  * @size: array size
+  */
+
+ void selection_sort(int *array, size_t size)
+ {
+ 	size_t i, j, min;
+
+	if (!array)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+			if (min != i)
+			{
+				swap(&array[min], &array[i]);
+				print_array(array, size);
+			}
+		}
+	}
+ }
